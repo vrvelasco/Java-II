0878bbb4df7a75ecbe6d4a25714c57151356b43e
Victor Velasco <victor.velasco@student.swic.edu>
2019-04-23 20:31:57 -0500

    Update BookPrinter.java

--- Apr 10 & 17/24 - Accessing Databases with JDBC/src/remijan/m/class2/lecture/BookPrinter.java
+++ Apr 10 & 17/24 - Accessing Databases with JDBC/src/remijan/m/class2/lecture/BookPrinter.java
@@ -2,20 +2,20 @@ package remijan.m.class2.lecture;
 
 import java.util.List;
 
-public class BookPrinter {
+public class DataPrinter {
 
-    public void print(List<Book> books) {
-        System.out.printf("\nBOOKS\n");
+    public void print(List<Names> names) {
+        System.out.printf("\nNames\n");
         System.out.printf("=====\n");
         books.forEach(b
             -> System.out.printf(
-                "%d\t %s\t %s\t %b\t %f\t %s\n",
+                "%d\t %s\t %s\t", // %b\t %f\t %s\n",
                 b.getId(),
                 b.getAuthor(),
                 b.getTitle(),
-                b.getBestSeller(),
-                b.getPrice().doubleValue(),
-                b.getPublicationDate()
+                //b.getBestSeller(),
+                //b.getPrice().doubleValue(),
+               // b.getPublicationDate()
             )
         );
         System.out.printf("=====\n");
